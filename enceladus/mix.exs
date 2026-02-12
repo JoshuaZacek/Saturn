3a265b5192b41bf4071acf20e63820b269d38fcc
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-02-12 11:23:01 +0000

    Update enceladus mix dependencies

--- enceladus/mix.exs
+++ enceladus/mix.exs
@@ -5,7 +5,7 @@ defmodule Saturn.MixProject do
     [
       app: :saturn,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.19",
       start_permanent: System.get_env("MIX_ENV") == "prod",
       deps: deps()
     ]
@@ -22,10 +22,10 @@ defmodule Saturn.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.8"},
       {:jason, "~> 1.2"},
-      {:postgrex, "~> 0.15.1"},
-      {:ecto_sql, "~> 3.7.0"},
+      {:postgrex, "~> 0.19"},
+      {:ecto_sql, "~> 3.13"},
       {:argon2_elixir, "~> 2.0"},
       {:corsica, "~> 1.1.3"},
       {:tarams, "~> 1.6"},
