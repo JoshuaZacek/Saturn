48aa799d702ec61256e7634604c82bf8c349fa96
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-02-25 03:10:15 +0000

    Another commit to upload file that were accidentally not staged

---
+++ titan/src/pages/AuthPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Authentication Page</h1>
+
+  <router-link to="/">Go back to Home</router-link>
+</template>
+
+<style scoped></style>
---
+++ titan/src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Home page</h1>
+
+  <p>Visit <router-link to="/auth">AuthPage</router-link></p>
+</template>
+
+<style scoped></style>
