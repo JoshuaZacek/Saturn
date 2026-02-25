c3c94d8b3937a0c3979c47de5ceebd5d50301302
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-02-25 03:06:47 +0000

    Created basic structure for web app

--- titan/src/router/index.ts
+++ titan/src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthPage from '../pages/AuthPage.vue'
+import HomePage from '../pages/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthPage,
+    },
+  ],
 })
 
 export default router
