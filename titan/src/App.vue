9ea0fba202c214ff6883953291e9a312c39759df
Joshua Zacek <personal.joshuazacek@gmail.com>
2026-04-06 21:45:56 +0100

    Initialized new Vue project

--- titan/src/App.vue
+++ titan/src/App.vue
@@ -1,52 +1,11 @@
+<script setup lang="ts"></script>
+
 <template>
-  <Navbar />
-  <div class="flexContainer">
-    <router-view :key="$route.fullPath" />
-  </div>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import Navbar from "@/components/Navbar.vue";
-
-@Options({
-  components: {
-    Navbar,
-  },
-})
-export default class App extends Vue {}
-</script>
-
-<style>
-:root {
-  --backgroundPrimary: #fcfcfc; /* the background colour */
-  --backgroundSecondary: #fff; /* colour for main elements against background */
-  --backgroundTertiary: #efefef; /* colur for elements against a main element */
-  --textPrimary: #000; /* primary text colour */
-  --textSecondary: #757575; /* secondary text colour */
-  --textTertiary: #d9d9d9; /* tertiary text colour */
-}
-
-body {
-  width: 100%;
-  height: 100%;
-  background: var(--backgroundPrimary);
-}
-
-* {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: "Helvetica Neue", Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-.flexContainer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-</style>
+<style scoped></style>
--- titan/src/router/index.ts
+++ titan/src/router/index.ts
@@ -1,71 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-import Moon from '../views/Moon.vue'
-import NotFound from '../views/404.vue'
-import CreateMoon from '../views/CreateMoon.vue'
-import CreatePost from '../views/CreatePost.vue'
-import Settings from '../views/Settings.vue'
-import PostWithComments from '../views/PostWithComments.vue'
-import Profile from '../views/Profile.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/404',
-    name: '404',
-    component: NotFound
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: Settings
-  },
-  {
-    path: '/create/moon',
-    name: 'CreateMoon',
-    component: CreateMoon
-  },
-  {
-    path: '/create/post',
-    name: 'CreatePost',
-    component: CreatePost
-  },
-  {
-    path: '/post/:id/:comment?',
-    name: "PostWithComments",
-    component: PostWithComments
-  },
-  {
-    path: '/@:username(.*)',
-    name: 'Profile',
-    component: Profile
-  },
-  {
-    path: '/:moon',
-    name: "Moon",
-    component: Moon,
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    component: NotFound
-  }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [],
 })
 
 export default router
